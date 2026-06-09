10185bd468fc3d61dc6bbd4a6b09a3ab3e8ee2f8
htp86 <htpspam86@htpmultimedia.fr>
2026-06-09 16:29:43 +0200

    Initial SAV86 module - version propre (token JIRA en base, .bak supprimes, .gitignore)

--- ajax/param_jira_direct.php
+++ ajax/param_jira_direct.php
@@ -1,8 +1,14 @@
 <?php
 // Version 20260422 Build 0911
-// /volume1/web/dolibarr_test/htdocs/custom/sav86/ajax/param_jira_direct.php
+// Inclusion Dolibarr pour lire la constante SAV86_JIRA_API_TOKEN en base
+if (!defined('DOL_INC_USED')) {
+    $res = 0;
+    if (!$res && file_exists("../../../main.inc.php")) $res = @include "../../../main.inc.php";
+    if (!$res && file_exists("../../main.inc.php")) $res = @include "../../main.inc.php";
+    if (!$res) { die("Include of main fails"); }
+}
 define('JIRA_BASE', 'https://htpmultimedia.atlassian.net');
 define('JIRA_USER_EMAIL', 'sav86@htpmultimedia.fr');
-define('JIRA_API_TOKEN', 'ATATT3xFfGF0IWL7mBKa0HG2eJwlNxzrYfct9PeL0i6MN5wpFX7CRAJ5UPCAXEqz7ac5rMc2--bR5HB8ZBLXN3dVuyp0FTZA45iZOUm1XpaCz4RLnmMUm9g7jQLr-7JFrRuIHmL56XabWXYtOsdbdIk2kTTyMO071sj3TbXn2slx7r2lUCJ_FgQ=08EA7751'); // Ton token en clair
+define('JIRA_API_TOKEN', dolibarr_get_const($db, 'SAV86_JIRA_API_TOKEN', 0)); // Stockee via admin/setup.php (roue dentee)
 define('JIRA_PROJECT_KEY', 'T86');
 define('JIRA_ISSUE_TYPE_ID', '10003');
